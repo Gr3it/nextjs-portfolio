c607b13c1cc11005f92f2d23c255ba3b2d8ef571
Emanuele Zini <emanuele.zini11@gmail.com>
2025-11-17 17:36:50 +0100

    optimized world sections

--- models/world/World.jsx
+++ models/world/World.jsx
@@ -1,24 +1,35 @@
 import React from 'react'
-import { Village } from './Village'
-import { Forest } from './Forest'
-import { Plains } from './Plains'
-import { Badlands } from './Badlands'
-import { Desert } from './Desert'
-import { Ocean } from './Ocean'
-import { Skylands } from './Skylands'
-import { City } from './City'
+import { useGLTF } from '@react-three/drei'
 
-export default function World() {
+export default function MyScene(props) {
+  const { scene:Village } = useGLTF('/models/world/Village-transformed.glb')
+  const { scene:Forest } = useGLTF('/models/world/Forest-transformed.glb')
+  const { scene:Plains } = useGLTF('/models/world/Plains-transformed.glb')
+  const { scene:Badlands } = useGLTF('/models/world/Badlands-transformed.glb')
+  const { scene:Desert } = useGLTF('/models/world/Desert-transformed.glb')
+  const { scene:Ocean } = useGLTF('/models/world/Ocean-transformed.glb')
+  const { scene:Skylands } = useGLTF('/models/world/Skylands-transformed.glb')
+  const { scene:City } = useGLTF('/models/world/City-transformed.glb')
+
   return (
-    <>
-        <Village position={[0,0,0]}/>
-        <Forest position={[0,0,32]}/>
-        <Plains position={[0,0,142]}/>
-        <Badlands position={[0,0,238]}/>
-        <Desert position={[0,0,286]}/>
-        <Ocean position={[0,0,334]}/>
-        <Skylands position={[0,0,484]}/>
-        <City position={[0,0,521]}/>
-    </>
+    <group {...props}>
+      <primitive object={Village} position={[0,0,0]}/>
+      <primitive object={Forest} position={[0,0,32]}/>
+      <primitive object={Plains} position={[0,0,142]}/>
+      <primitive object={Badlands} position={[0,0,238]}/>
+      <primitive object={Desert} position={[0,0,286]}/>
+      <primitive object={Ocean} position={[0,0,334]}/>
+      <primitive object={Skylands} position={[0,0,484]}/>
+      <primitive object={City} position={[0,0,521]}/>
+    </group>
   )
 }
+
+useGLTF.preload('/models/world/Village-transformed.glb')
+useGLTF.preload('/models/world/Forest-transformed.glb')
+useGLTF.preload('/models/world/Plains-transformed.glb')
+useGLTF.preload('/models/world/Badlands-transformed.glb')
+useGLTF.preload('/models/world/Desert-transformed.glb')
+useGLTF.preload('/models/world/Ocean-transformed.glb')
+useGLTF.preload('/models/world/Skylands-transformed.glb')
+useGLTF.preload('/models/world/City-transformed.glb')
